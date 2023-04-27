2f105c49d5e481a8129f9aeb35fb51936f4937b3
kunalsathe18 <kunalsathe18@gmail.com>
2023-04-27 11:42:30 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+/*Js Methods*/
+/*length*/
+let name="kunal";
+console.log(name.length);
+
+/*to uppercase*/
+let startname="kunal";
+console.log(startname.toUpperCase());
+
+/*to lowercase*/
+let firstname="KUNAL";
+console.log(firstname.toLowerCase());
+
+/*slice*/
+var number="kunal";
+ console.log(number.slice(2,4));
+
+/*slice*/
+var number="kunal";
+console.log(number.slice(2));
+
+/*replace*/
+var number="kunal";
+console.log(number.replace("kunal","prasad"));
+
+/*concat*/
+let name_1="kunal";
+let name_2="sathe";
+console.log(name_1.concat(name_2));
+
+/*trim*/
+let kunal="kunal";
+console.log(kunal.trim());
+
+/*includes*/
+let word="Javascript is a great language.";
+console.log(word.includes("is"));
+
+/*startswith*/
+let start="Javascript is a great language.";
+console.log(start.startsWith("Javascript"));
+
+/*endsswith*/
+let end="Javascript is a great language.";
+console.log(end.endsWith("."));
+
+/*search*/
+const searching="Javascript is a great language.";
+console.log(searching.search("great"));
+
+/*match*/
+const matchword="I know Who i am";
+console.log(matchword.match(/i/g));
+
+/*index of()*/
+const indexing="Javascript is a great language.";
+console.log(indexing.indexOf("."));
+
+/*last index of()*/
+const indextwo="Javascript is a great language.";
+console.log(indextwo.lastIndexOf("a"));
+
+/*charcode At()*/
+const code="Developer";
+console.log(code.charCodeAt(1));
+
+/*chraft*/
+//const javaname="Javascript";//
+//console.log(javaname.charft(3));//
+
+/*fromcharcode*/
+
+//var form="kunal";
+//console.log(form.fromcharcode(90));//
+
+/*split*/
+let a="javasacript";
+console.log(a.split("a"));
+
+/*repeat*/
+let b="18";
+console.log(b.repeat(10));
+
+/*substr*/
+let c="javascript";
+console.log(c.substr(3,5));
+
+/*substring same work as slice method*/
+
+/*to string*/
+var ks="sathe";
+var sk="kunal";
+console.log(sk+ks);
+
+/*value of ()*/
+let ab="javascript";
+console.log(ab.valueOf());
